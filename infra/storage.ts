b64fa0f1bcf9ad106413eda4f585829a1edb9e26
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2021-11-29 15:36:00 +0000

    refactor: Cleanup old resources

--- infra/storage.ts
+++ infra/storage.ts
@@ -14,3 +14,5 @@ export function createS3Bucket(name: string) {
 # 2021-10-04 - fix: Resolve IAM permissions
 
 # 2021-10-18 - refactor: Cleanup old resources
+
+# 2021-11-29 - refactor: Cleanup old resources
