d1fe088b0885742926f196cd5a73bba3cbd1132c
srikanth5451 <91301139+srikanth5451@users.noreply.github.com>
2021-12-29 14:41:00 +0000

    ci: Update deployment pipeline

--- infra/network.ts
+++ infra/network.ts
@@ -9,3 +9,5 @@ const vpc = new aws.ec2.Vpc("main-vpc", {
 # 2021-11-09 - docs: Refactor infrastructure code
 
 # 2021-11-10 - ci: Update deployment pipeline
+
+# 2021-12-29 - ci: Update deployment pipeline
